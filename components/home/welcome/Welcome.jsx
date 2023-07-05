c7ccbc3f94f40b75d39a81a3362fd84c16b7022c
Marc Valeros <marcnathanielvaleros@gmail.com>
2023-07-05 18:58:00 +0800

    Update Welcome.jsx

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -17,13 +17,17 @@ const Welcome = () => {
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
           <TextInput
-          style={styles.searchInput}
-          value=""
-          onChange={() => {}}
-          placeholder="What are you looking for? "
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for? "
           />
-
         </View>
+
+          <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+            <Image source={icons.search} resizeMode="contain" style={styles.searchBtnImage} />
+          </TouchableOpacity>
+
       </View>
     </View>
   )
