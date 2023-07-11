dc720f2338fa8fa9b26409d501e7102dfc967a11
Marc Valeros <marcnathanielvaleros@gmail.com>
2023-07-11 14:50:26 +0800

    Update Welcome.jsx

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -5,8 +5,11 @@ import { useRouter } from 'expo-router'
 import styles from './welcome.style'
 import {icons, SIZES} from '../../../constants'
 
+const jobTypes = ["Full-time","Part-time","Contractor"]
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time')
 
   return (
     <View>
@@ -27,7 +30,24 @@ const Welcome = () => {
           <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
             <Image source={icons.search} resizeMode="contain" style={styles.searchBtnImage} />
           </TouchableOpacity>
+      </View>
 
+      <View style={styles.tabsContainer}>
+         <FlatList 
+            data={ jobTypes }   
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={styles.tab(activeJobType, item)}
+                onPress={() =>{
+                  setActiveJobType(item);
+                  router.push(`/search/${item}`)
+                  }
+                }
+              >
+                <Text style={styles.tabText(activeJobType,item)}>{ item }</Text>
+              </TouchableOpacity>
+            )} 
+          />
       </View>
     </View>
   )
