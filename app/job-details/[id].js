8210b0250c1fb1249381cdff583836adbdf39238
Marc Valeros <marcnathanielvaleros@gmail.com>
2023-07-17 23:56:21 +0800

    Update [id].js

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -52,7 +52,7 @@ const JobDetails = () => {
             ): (
               <View style ={{ padding: SIZES.medium, paddingBottom: 100}}>
                 <Company
-                  companyLogo={data[0].employer_log0}
+                  companyLogo={data[0].employer_logo}
                   jobTitle={data[0].job_title}
                   companyName={data[0].employer_name}
                   location={data[0].job_country}
