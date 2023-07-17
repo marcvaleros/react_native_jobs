555a5b534173a6ccbff83266e6f5e946904db7db
Marc Valeros <marcnathanielvaleros@gmail.com>
2023-07-17 23:27:18 +0800

    Update [id].js

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,6 +1,6 @@
 import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from "react-native"
 import { Stack,useRouter, useSearchParams } from "expo-router"
-import { useCallback, useSate } from "react"
+import { useCallback, useState } from "react"
 
 import {Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics} from '../../components';
 import {COLORS, SIZES, icons} from '../../constants'
@@ -9,34 +9,43 @@ import useFetch from "../../hook/useFetch";
 const JobDetails = () => {
   const params = useSearchParams();
   const router = useRouter();
+  const [refreshing, setRefreshing] = useState(false);
+
   const { data, isLoading, error, refetch} = useFetch('job-details',{ job_id: params.id })
+  const onRefresh = () => {};
 
   return (
     <SafeAreaView style={{ flex: 1 , backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
         options={{
-          headerStyle: {backgroundColor: COLORS.lightWhite },
+          headerStyle: { backgroundColor: COLORS.lightWhite },
           headerShadowVisible: false,
           headerBackVisible:false,
-          headerLeft: () => {
+          headerLeft: () => (
             <ScreenHeaderBtn 
               iconUrl={icons.left}
               dimension="60%"
               handlePress={() => router.back()}
               
             />
-          },
-          headerRight: () => {
+          ),
+          headerRight: () => (
             <ScreenHeaderBtn 
               iconUrl={icons.share}
               dimension="60%"
             />
-          }
-
+          ),
+          headerTitle: ''
         }}
       >
-
       </Stack.Screen>
+
+      <>
+          <ScrollView showsVerticalScrollIndicator={false} refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }>
+          </ScrollView>
+      </>
     </SafeAreaView>
   )
 }
